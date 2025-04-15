19cd8f10bc9bd12ec61ee2dd822b2d78a6a79c52
avinash <2100049032ece@gmail.com>
2025-04-15 18:21:49 +0530

    Added Modal  Project

--- Modal/final/script.js
+++ Modal/final/script.js
@@ -1,16 +1,17 @@
-const modelBtn = document.querySelector(".modal_btn");
-const closeBtn = document.querySelector(".close_btn");
-const overlay = document.querySelector(".overlay");
-modelBtn.addEventListener("click", function () {
-  overlay.classList.add("active");
+/* MODAL COMPONENT */
+const modelBtn = document.querySelector('.modal_btn');
+const closeBtn = document.querySelector('.close_btn');
+const overlay = document.querySelector('.overlay');
+modelBtn.addEventListener('click', function () {
+  overlay.classList.add('active');
 });
 
-closeBtn.addEventListener("click", function () {
-  overlay.classList.remove("active");
+closeBtn.addEventListener('click', function () {
+  overlay.classList.remove('active');
 });
 
-overlay.addEventListener("click", function (e) {
+overlay.addEventListener('click', function (e) {
   if (e.target === overlay) {
-    overlay.classList.remove("active");
+    overlay.classList.remove('active');
   }
 });
